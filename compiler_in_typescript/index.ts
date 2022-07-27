3b914bc6f7e33d1cf1a0897afee281177b2423a5
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-07-27 16:37:33 +0600

    compiler-in-typescript

---
+++ compiler_in_typescript/index.ts
@@ -0,0 +1,5 @@
+function summation(a: number, b:number):number {
+    return a + b;
+}
+
+console.log(summation(5, 6));
---
+++ compiler_in_typescript/output/index.js
@@ -0,0 +1,4 @@
+function summation(a, b) {
+    return a + b;
+}
+console.log(summation(5, 6));
