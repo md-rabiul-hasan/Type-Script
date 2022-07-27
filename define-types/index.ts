e5f3730d47b2ee8bff3b4c6456009ea8846de4a9
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2022-07-27 17:11:00 +0600

    define types

---
+++ define-types/index.ts
@@ -0,0 +1,18 @@
+let my_name: string     = "Rabiul Hasan";
+let my_age: number      = 26;
+let is_married: boolean = false;
+let no_value            = null;
+// undefined;
+// symbol
+
+// array
+let friends:string[]      = ['Eyakub', 'Forid', 'Hasan'];
+let friends_age: number[] = [1,2,3];
+let random_array: any[]   = ['Eyakub', 5,true];
+
+// function 
+let myFunction = (name:string):string => {
+    return name;
+}
+
+console.log(myFunction("Rabiul Hasan"));
